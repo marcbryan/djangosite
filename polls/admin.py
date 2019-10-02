ee714a7c6843314e36ed37f73955f12956c06cc3
marcbryan <marcboakyeflores98@gmail.com>
2019-10-02 20:27:32 +0200

    Tuto 2 finalizado

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from.models import Question, Answer, Vote
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Vote)
+
 # Register your models here.
