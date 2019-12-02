a9111dc151ce8bc1d1f914d541e886cb0f9d12e8
marcbryan <marcboakyeflores98@gmail.com>
2019-12-02 17:01:42 +0100

    Tuto 6 done (falta cambiar results de views.py)

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-from .models import Question, Answer
+from .models import Question, Answer, Vote
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -32,15 +34,20 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+        selected_choice = question.answer_set.get(pk=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        #selected_choice.votes += 1
+        #selected_choice.save()
+        vote = Vote()
+        vote.answer_id = selected_choice
+        vote.date_time = timezone.now()
+        vote.location = "Cornellà de Llobregat"
+        vote.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     #return HttpResponse("You're voting on question %s." % question_id)
 
@@ -66,3 +73,23 @@ def llista(request):
                         "elements":items
                     }
                 )
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Devuelve las últimas 5 preguntas
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+#def vote(request, question_id):
